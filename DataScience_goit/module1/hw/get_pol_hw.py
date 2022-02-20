0bab86f765b7f47bf1b2dbb80bc3d5ad5f26ea24
rostykpython <89851597+rostykpython@users.noreply.github.com>
2022-02-20 18:12:09 +0200

    Update get_pol_hw.py

--- DataScience_goit/module1/hw/get_pol_hw.py
+++ DataScience_goit/module1/hw/get_pol_hw.py
@@ -2,18 +2,19 @@ import numpy as np
 
 
 def get_polynom(coords):
-    len_points = len(coords) - 1
+    len_points = len(coords)
 
-    a = np.zeros((len_points + 1, len_points + 1))
+    a = np.zeros((len_points, len_points))
+    b = np.zeros((len_points, 1))
+
     for row, coord in enumerate(coords):
+        len_points -= 1
         for ind in range(len(coords)):
             a[row][ind] = coord[0] ** len_points
             len_points -= 1
-        len_points = len(coords) - 1
-
-    b = np.array([coord[1] for coord in coords]).reshape(len_points + 1, 1)
-
+        len_points = len(coords)
+        b[row][0] = coord[1]
     return np.linalg.solve(a, b)
 
 
-print(get_polynom([(1, 12), (3, 54), (-1, 2), (5, 123), (7, 234)]))
+print(get_polynom([(1, 12), (3, 54), (-1, 2)]))
